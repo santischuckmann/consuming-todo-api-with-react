0f7b6f66125d9b641342c63ce795cf07d7d33a50
Santiago <schuckmannsantiago@gmail.com>
2022-04-17 00:26:39 -0300

    added add and delete buttons

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Editing from './Editing'
 import Task from './Task'
 import axios from 'axios';
@@ -10,32 +10,59 @@ const api = axios.create({
 const Main = () => {
   const [showEdit, setShowEdit] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const taskName = useRef();
+  const updatedTaskName = useRef();
 
   const getTasks = async () => {
-    const response = await fetch ("https://to-do-node-mysql.herokuapp.com/api/tasks");
-    const data = await response.json()
-    setTasks (data)
+    try {
+      const response = await api.get('/');
+      setTasks(response.data.tasks)
+      console.log(response.data.tasks)
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data)
+      } else {
+        console.log(`Error: ${err.message}`)
+      }
+    }
   }
 
   const addTask = async () => {
-    const response = api.post('/', {name: "Ramiro"})
-    setTasks(...tasks, response)
+    const name =  {name: taskName.current.value}
+    await api.post('/', name)
+    setTasks([...tasks, name])
+    taskName.current.value = ""
   }
 
-   useEffect( () => {
+  const deleteTask = async (id) => {
+    await api.delete(`/${id}`)
+    const newTasks = tasks.filter((task) => {
+      if (task.id !== id) {
+        return task
+      }
+    })
+    setTasks(newTasks)
+  }
+
+  const updateTask = async (id) => {
+    const name = {name: updatedTaskName.current.value}
+    await api.patch(`/${id}`, )
+  }
+
+   useEffect(() => {
     getTasks();
-  }, [tasks])
+  }, [])
 
 
   return (
     <div className = "to_do_list_container">
       <h1>To-Do List</h1>
-      {tasks.tasks?.map((task) => {
-        return <Task key = {task.id} name = {task.name}></Task>
+      {tasks.map((task) => {
+        return <Task key = {task.id} name = {task.name} deleteTask = {deleteTask} {...task}></Task>
       })}
       <div className = "main_input">
-      <input type ="text"></input>
-      <button onClick = {addTask}>Add</button>
+      <input type ="text" ref ={taskName}></input>
+      <button onClick={addTask}>Add</button>
       </div>
       <Editing />
     </div>
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Task = (props) => {
+const Task = ({name, id, deleteTask}) => {
   return (
     <div className='task'>
       <input type ="checkbox" id = "checkbox"/>
-      <label htmlFor= "checkbox">{props.name}</label>
+      <label htmlFor= "checkbox">{name}</label>
       <a href="#">Edit</a>
+      <button onClick = {() => deleteTask(id)}>Delete</button>
     </div>
   )
 }
