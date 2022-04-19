9fc43eaf90c4ca9a24bd4bd42322e8057773254d
Santiago <schuckmannsantiago@gmail.com>
2022-04-18 22:09:19 -0300

    all functionality in place, left problem with patch

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,7 +7,7 @@ const api = axios.create({
 })
 
 const Main = () => {
-  const [showEdit, setShowEdit] = useState({state: false, editName: "", editId: 0});
+  const [manageEdition, setManageEdition] = useState({state: false, editName: "", editId: 0});
   const [tasks, setTasks] = useState([]);
   const taskName = useRef();
   const updatedTaskName = useRef();
@@ -16,6 +16,7 @@ const Main = () => {
     try {
       const response = await api.get('/');
       setTasks(response.data.tasks)
+      console.log(response.data.tasks)
     } catch (err) {
       if (err.response) {
         console.log(err.response.data)
@@ -41,33 +42,28 @@ const Main = () => {
     setTasks(newTasks)
   }
 
-  const findTaskById = (id) => {
-    
-  }
-
   const editTask = (id) => {
     const taskAboutToEdit = tasks.find((task) => {
       return task.id === id
     })
-    setShowEdit({state: true, editName: taskAboutToEdit.name, editId: id})
+    setManageEdition({state: true, editName: taskAboutToEdit.name, editId: id})
   }
 
   const updateTask = async (id) => {
     const taskAboutToEdit = tasks.find((task) => {
       return task.id === id
     })
-    setShowEdit({state: true, editName: taskAboutToEdit.name, editId: id})
-    const newEditedName = {name : updatedTaskName.current.value}
-    await api.patch(`/${id}`, {name: newEditedName})
-    const clonedTasks = [...tasks]
-    const index = clonedTasks.indexOf({id: id})
-    clonedTasks[index] = {...taskAboutToEdit}
-    clonedTasks[index].name = newEditedName;
+    // await api.patch(`/${id}`, {name: newEditedName})
+    const clonedTasks = tasks
+    const indexOfTask = clonedTasks.findIndex(task => task.id === id)
+    console.log(indexOfTask)
+    clonedTasks[indexOfTask] = {...taskAboutToEdit}
+    console.log(clonedTasks[indexOfTask])
+    clonedTasks[indexOfTask].name = updatedTaskName.current.value;
     setTasks(clonedTasks)
     updatedTaskName.current.value = "";
+    setManageEdition({state: false, editName: "", editId: 0})
   }
-
-  
 
    useEffect(() => {
     getTasks();
@@ -84,12 +80,12 @@ const Main = () => {
       <input type ="text" ref ={taskName}></input>
       <button onClick={addTask}>Add</button>
       </div>
-      {showEdit.state && 
+      {manageEdition.state && 
       <div className='editing_task'>
-        <h2>Editing task "{showEdit.editName}"</h2>
+        <h2>Editing task "{manageEdition.editName}" de id {manageEdition.editId}</h2>
         <input type="text" ref = {updatedTaskName}></input>
         <div className='editing_task_buttons'> 
-          <button>Save</button>
+          <button onClick = {() => updateTask(manageEdition.editId)}>Save</button>
           <button>Cancel</button>
         </div>
       </div>}
