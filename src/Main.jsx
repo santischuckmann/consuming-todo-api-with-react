d84b7ab4b46e4ec9bed299a8b5ef72efbae1eab8
Santiago <schuckmannsantiago@gmail.com>
2022-04-20 16:52:33 -0300

    finished but checkbox functionality

--- src/Main.jsx
+++ src/Main.jsx
@@ -53,17 +53,19 @@ const Main = () => {
     const taskAboutToEdit = tasks.find((task) => {
       return task.id === id
     })
-    // await api.patch(`/${id}`, {name: newEditedName})
+    let newEditedName = updatedTaskName.current.value;
+    await api.patch(`/${id}`, {name: newEditedName})
     const clonedTasks = tasks
     const indexOfTask = clonedTasks.findIndex(task => task.id === id)
     console.log(indexOfTask)
     clonedTasks[indexOfTask] = {...taskAboutToEdit}
     console.log(clonedTasks[indexOfTask])
-    clonedTasks[indexOfTask].name = updatedTaskName.current.value;
+    clonedTasks[indexOfTask].name = newEditedName;
     setTasks(clonedTasks)
     updatedTaskName.current.value = "";
     setManageEdition({state: false, editName: "", editId: 0})
   }
+
 
    useEffect(() => {
     getTasks();
@@ -73,9 +75,11 @@ const Main = () => {
   return (
     <div className = "to_do_list_container">
       <h1>To-Do List</h1>
+      <div className='tasks'>
       {tasks.map((task) => {
         return <Task key = {task.id} name = {task.name} deleteTask = {deleteTask} editTask = {editTask} {...task}></Task>
       })}
+      </div>
       <div className = "main_input">
       <input type ="text" ref ={taskName}></input>
       <button onClick={addTask}>Add</button>
@@ -86,7 +90,7 @@ const Main = () => {
         <input type="text" ref = {updatedTaskName}></input>
         <div className='editing_task_buttons'> 
           <button onClick = {() => updateTask(manageEdition.editId)}>Save</button>
-          <button>Cancel</button>
+          <button onClick = {() => setManageEdition({state: false})}> Cancel</button>
         </div>
       </div>}
     </div>
--- src/Task.jsx
+++ src/Task.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const Task = ({name, id, deleteTask, editTask}) => {
   return (
     <div className='task'>
-      <input type ="checkbox" id = "checkbox"/>
-      <label htmlFor= "checkbox">{name}</label>
-      <button onClick = {() => editTask(id)}>Edit</button>
-      <button style = {{background: "turquoise"}} onClick = {() => deleteTask(id)}>Delete</button>
+      <div className='nameTasks'>
+        <input type ="checkbox" id = "checkbox"/>
+        <label htmlFor= "checkbox">{name}</label>
+      </div>
+        <button onClick = {() => editTask(id)}>Edit</button>
+        <button style = {{background: "turquoise"}} onClick = {() => deleteTask(id)}>Delete</button>
     </div>
   )
 }
